a41100c77f05bfcfec5b509f3ce261d0b946392c
OssiB <ossi.bister@gmail.com>
2015-09-08 00:42:46 +0300

    formatting

--- src/test/java/TestTopTitleMapper.java
+++ src/test/java/TestTopTitleMapper.java
@@ -10,31 +10,30 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-
-
-
-
-
 public class TestTopTitleMapper {
 	MapDriver<Text, Text, NullWritable, TopTitles.TextArrayWritable> mapDriver;
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		TopTitles.TopTitlesMap mapper = new TopTitles.TopTitlesMap();
 		mapDriver = MapDriver.newMapDriver(mapper);
 		Configuration conf = mapDriver.getConfiguration();
-		conf.set("N","2");
+		conf.set("N", "2");
 	}
+
 	@Test
-	public void processRecord() throws IOException{
-	    mapDriver.withInput(new Text("student"),new Text("2"));
-	    mapDriver.withInput(new Text("professor"),new Text("2"));
-	    mapDriver.withInput(new Text("school"),new Text("1"));
-	    mapDriver.withInput(new Text("answer"),new Text("3"));
-		List<Pair<NullWritable, TopTitles.TextArrayWritable>> result=mapDriver.run();
-		assertEquals(new Text("(2, student)"),(Text)result.get(0).getSecond().get()[0]);
-		assertEquals(new Text("(3, answer)"),(Text)result.get(0).getSecond().get()[1]);
-		
+	public void processRecord() throws IOException {
+		mapDriver.withInput(new Text("student"), new Text("2"));
+		mapDriver.withInput(new Text("professor"), new Text("2"));
+		mapDriver.withInput(new Text("school"), new Text("1"));
+		mapDriver.withInput(new Text("answer"), new Text("3"));
+		List<Pair<NullWritable, TopTitles.TextArrayWritable>> result = mapDriver
+				.run();
+		assertEquals(new Text("(2, student)"), (Text) result.get(0).getSecond()
+				.get()[0]);
+		assertEquals(new Text("(3, answer)"), (Text) result.get(0).getSecond()
+				.get()[1]);
+
 	}
-    
+
 }
